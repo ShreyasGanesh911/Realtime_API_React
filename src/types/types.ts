4db652d0aa40b9b49cdf3252bf24c433e7ab4ca9
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-04-18 18:36:31 +0530

    defined types

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export type EventMessage = {
+    event_id?: string;
+    timestamp?: string;
+    [key: string]: any;
+  };
+ export interface AssistantMessage {
+    type:string
+    response:{
+      output: {
+        content: {
+          type: string;
+          transcript: string;
+        }[];
+        
+      }[];
+    }
+  }
+ export interface Message {
+    text: string;
+    isUser: boolean;
+    timestamp?: Date;
+  }
