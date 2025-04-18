8ca25c831689ea712b217b045fecdb8e8b2dfe8c
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-04-18 18:36:24 +0530

    defined components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 export default function Button({ icon, children, onClick, className = "" }: ButtonProps) {
   return (
     <button
-      className={`rounded-full p-4 flex border border-blue-200 items-center gap-1 hover:opacity-90 ${className} font-semibold`}
+      className={`rounded-full p-4 flex border border-blue-200 items-center gap-1 hover:opacity-90 hover:cursor-pointer ${className} font-semibold`}
       onClick={onClick}
     >
       {icon}
---
+++ src/components/MessageBubble.tsx
@@ -0,0 +1,39 @@
+import { marked } from "marked";
+
+interface Message {
+    text: string;
+    isUser: boolean;
+    timestamp?: Date;
+  }
+type Props = {
+    index: number
+    message:Message
+}
+
+function MessageBubble({index,message}:Props) {
+    const renderMessage = message.isUser ? message.text : marked(message.text);
+  return (
+    <>
+        <div
+              key={index}
+              className={`flex ${message.isUser ? 'justify-end' : 'justify-start'} mb-4`}
+            >
+              <div
+                className={`max-w-[70%] p-4 rounded-2xl shadow-sm
+                  ${message.isUser 
+                    ? 'bg-blue-600 text-white rounded-br-none' 
+                    : 'bg-gray-100 text-gray-800 rounded-bl-none'
+                  }`}
+              >
+                {message.isUser ? (
+                  message.text
+                ) : (
+                  <div dangerouslySetInnerHTML={{ __html: message.text }} />
+                )}
+              </div>
+            </div>
+    </>
+  )
+}
+
+export default MessageBubble
