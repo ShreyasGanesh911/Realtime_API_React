9d0498fa450cf2a14c9c79e278866f0a36af238f
ShreyasGanesh911 <shreyas.ganesh911@gmail.com>
2025-04-21 13:59:01 +0530

    added static serve

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import { sales_script } from './assets/script.js';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 const apiKey = process.env.OPENAI_API_KEY;
 const port = 8000
 app.get("/token", async (req, res) => {
@@ -13,8 +18,7 @@ app.get("/token", async (req, res) => {
         method: "POST",
         headers: {
           Authorization: `Bearer ${apiKey}`,
-          "Content-Type": "application/json",
-          response_format: "html", 
+          "Content-Type": "application/json"
         },
         body: JSON.stringify({
           model: "gpt-4o-realtime-preview-2024-12-17",
@@ -31,6 +35,11 @@ app.get("/token", async (req, res) => {
     }
   });
   
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  });
+
   app.listen(port, () => {
     console.log(`Backend running on http://localhost:${port}`);
   });
